409586a96eeacafc2c8b07429bf3c8cbac393ac4
George Zeng <georgejzeng@gmail.com>
2018-10-10 16:39:02 -0400

    Working script to get individual game summary

---
+++ src/get_game_summary.py
@@ -0,0 +1,43 @@
+import os, errno
+import json
+
+import pandas as pd
+
+import requests
+import time
+from bs4 import BeautifulSoup as bs
+
+# get scraped URLs
+boxscore_urls_path = '../data/boxscore_urls.json'
+with open(boxscore_urls_path, 'r') as infile:
+    boxscore_urls = json.load(infile)
+
+# make folder for storing results
+output_folder = '../data/BOS'
+try:
+    os.makedirs(output_folder)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+# get game page soup
+game_url = boxscore_urls[0]
+page = requests.get(game_url)
+soup = bs(page.content, 'html.parser')
+
+# get boxscore table
+boxscore_wrapper = soup.find('div', {'class': 'linescore_wrap'})
+boxscore_table = boxscore_wrapper.find('table')
+df_box = pd.read_html(str(boxscore_table))[0]
+
+# get a good filename using the URL
+output_fn = ('/'.join(
+    game_url.split('.')[-2].split('/')[-2:]).replace('/', '-')
+    + '.csv'
+)
+output_fp = os.path.join(output_folder, output_fn)
+print('Output file path:', output_fp)
+
+# write to csv
+if not os.path.exists(output_fp):
+    df_box.to_csv(output_fp)
