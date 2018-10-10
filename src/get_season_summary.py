8290e36d416c3b82188d99f53c6cbc93508b6ac3
George Zeng <georgejzeng@gmail.com>
2018-10-10 17:18:04 -0400

    Add writing to team folder for season scraper

--- src/get_season_summary.py
+++ src/get_season_summary.py
@@ -1,3 +1,4 @@
+import os, errno
 import json
 
 import pandas as pd
@@ -10,12 +11,22 @@ url = 'https://www.baseball-reference.com/teams/BOS/2018-schedule-scores.shtml'
 page = requests.get(url)
 soup = bs(page.content, 'html.parser')
 
+# make folder for storing results
+team_abbrev = url.split('.')[-2].split('/')[-2]
+output_folder = os.path.join('../data/', team_abbrev)
+
+try:
+    os.makedirs(output_folder)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
 # find and convert table
 table_wrapper = soup.find('div', {'id': 'all_team_schedule'})
 table_html = str(table_wrapper.find('table'))
 df_table = pd.read_html(table_html)[0]
 
-table_output_path = '../data/season_summary.csv'
+table_output_path = os.path.join(output_folder, 'season_summary.csv')
 df_table.to_csv(table_output_path)
 
 # get links to each game summary
@@ -25,6 +36,6 @@ boxscore_url_suffixes = [box_elem.find('a')['href'] for box_elem in boxscore_ele
 base_url = '/'.join(url.split('/')[:3])
 boxscore_urls = ['/'.join([base_url, suffix]) for suffix in boxscore_url_suffixes]
 
-boxscore_urls_path = '../data/boxscore_urls.json'
+boxscore_urls_path = os.path.join(output_folder, 'boxscore_urls.json')
 with open(boxscore_urls_path, 'w') as outfile:
     json.dump(boxscore_urls, outfile)
