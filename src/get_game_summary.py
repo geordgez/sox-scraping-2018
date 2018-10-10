e3f5b8cd392371e7a35a18a25266e8b76f3edf3d
George Zeng <georgejzeng@gmail.com>
2018-10-10 16:45:02 -0400

    Add randomly timed loop over all games

--- src/get_game_summary.py
+++ src/get_game_summary.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 
 import requests
+import random
 import time
 from bs4 import BeautifulSoup as bs
 
@@ -20,24 +21,30 @@ except OSError as e:
     if e.errno != errno.EEXIST:
         raise
 
-# get game page soup
-game_url = boxscore_urls[0]
-page = requests.get(game_url)
-soup = bs(page.content, 'html.parser')
+# loop over all game URLs:
+for game_url in boxscore_urls:
 
-# get boxscore table
-boxscore_wrapper = soup.find('div', {'class': 'linescore_wrap'})
-boxscore_table = boxscore_wrapper.find('table')
-df_box = pd.read_html(str(boxscore_table))[0]
+    # get game page soup
+    page = requests.get(game_url)
+    soup = bs(page.content, 'html.parser')
 
-# get a good filename using the URL
-output_fn = ('/'.join(
-    game_url.split('.')[-2].split('/')[-2:]).replace('/', '-')
-    + '.csv'
-)
-output_fp = os.path.join(output_folder, output_fn)
-print('Output file path:', output_fp)
+    # get boxscore table
+    boxscore_wrapper = soup.find('div', {'class': 'linescore_wrap'})
+    boxscore_table = boxscore_wrapper.find('table')
+    df_box = pd.read_html(str(boxscore_table))[0]
 
-# write to csv
-if not os.path.exists(output_fp):
-    df_box.to_csv(output_fp)
+    # get a good filename using the URL
+    output_fn = ('/'.join(
+        game_url.split('.')[-2].split('/')[-2:]).replace('/', '-')
+        + '.csv'
+    )
+    output_fp = os.path.join(output_folder, output_fn)
+    print('Output file path:', output_fp)
+
+    # write to csv
+    if not os.path.exists(output_fp):
+        df_box.to_csv(output_fp)
+
+    # wait a bit
+    time_to_wait = random.uniform(0.5, 6.5)
+    time.sleep(time_to_wait)
