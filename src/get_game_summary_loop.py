dcc5a2e6243a112ef2f7ca9576cbe905d8078bc8
George Zeng <georgejzeng@gmail.com>
2018-10-17 15:24:38 -0400

    Make game data loop over all teams

--- src/get_game_summary_loop.py
+++ src/get_game_summary_loop.py
@@ -8,49 +8,57 @@ import random
 import time
 from bs4 import BeautifulSoup as bs
 
-team_abbrev = 'BOS'
-input_folder = '../data/'
+# get list of all team URLs
+teams_list_urls_path = '../data/all_teams_list.json'
+with open(teams_list_urls_path, 'r') as infile:
+    team_urls = json.load(infile)
 
-# make folder for storing results
-output_folder = '../data/' + team_abbrev + '/gamedata'
-try:
-    os.makedirs(output_folder)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
+team_abbrevs = [turl.split('.')[-1].split('/')[-1] for turl in team_urls]
 
-# get scraped URLs
-boxscore_urls_path = os.path.join(input_folder, 'boxscore_urls.json')
-with open(boxscore_urls_path, 'r') as infile:
-    boxscore_urls = json.load(infile)
+for team_abbrev in team_abbrevs:
 
-# loop over all game URLs:
-for game_url in boxscore_urls:
+    input_folder = '../data/' + team_abbrev
+    
+    # make folder for storing results
+    output_folder = '../data/' + team_abbrev + '/gamedata'
+    try:
+        os.makedirs(output_folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
-    # get a good filename using the URL
-    output_fn = ('/'.join(
-        game_url.split('.')[-2].split('/')[-2:]).replace('/', '-')
-        + '.csv'
-    )
-    output_fp = os.path.join(output_folder, output_fn)
-    print('Output file path:', output_fp)
+    # get scraped URLs
+    boxscore_urls_path = os.path.join(input_folder, 'boxscore_urls.json')
+    with open(boxscore_urls_path, 'r') as infile:
+        boxscore_urls = json.load(infile)
 
-    # don't scrape if the file already exists
-    if os.path.exists(output_fp):
-        continue
+    # loop over all game URLs:
+    for game_url in boxscore_urls:
 
-    # get game page soup
-    page = requests.get(game_url)
-    soup = bs(page.content, 'html.parser')
+        # get a good filename using the URL
+        output_fn = ('/'.join(
+            game_url.split('.')[-2].split('/')[-2:]).replace('/', '-')
+            + '.csv'
+        )
+        output_fp = os.path.join(output_folder, output_fn)
+        print('Output file path:', output_fp)
 
-    # get boxscore table
-    boxscore_wrapper = soup.find('div', {'class': 'linescore_wrap'})
-    boxscore_table = boxscore_wrapper.find('table')
-    df_box = pd.read_html(str(boxscore_table))[0]
+        # don't scrape if the file already exists
+        if os.path.exists(output_fp):
+            continue
 
-    # write to csv
-    df_box.to_csv(output_fp)
+        # get game page soup
+        page = requests.get(game_url)
+        soup = bs(page.content, 'html.parser')
 
-    # wait a bit
-    time_to_wait = min(10, 0.25 + random.expovariate(2))
-    time.sleep(time_to_wait)
+        # get boxscore table
+        boxscore_wrapper = soup.find('div', {'class': 'linescore_wrap'})
+        boxscore_table = boxscore_wrapper.find('table')
+        df_box = pd.read_html(str(boxscore_table))[0]
+
+        # write to csv
+        df_box.to_csv(output_fp)
+
+        # wait a bit
+        time_to_wait = min(10, 0.25 + random.expovariate(2))
+        time.sleep(time_to_wait)
