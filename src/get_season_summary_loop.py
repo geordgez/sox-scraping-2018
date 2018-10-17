3b6d013a63fdd931e5a060295db5ce48e5cf7bab
George Zeng <georgejzeng@gmail.com>
2018-10-17 14:21:28 -0400

    Implement looping over all team summaries
    - HACK: quickly rewrote the method of getting the team abbreviation (team_abbrev) with different indices
      - URL is also hard-coded in

--- src/get_season_summary_loop.py
+++ src/get_season_summary_loop.py
@@ -6,36 +6,48 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
 
+# get list of all team URLs
+teams_list_urls_path = '../data/all_teams_list.json'
+with open(teams_list_urls_path, 'r') as infile:
+    team_urls = json.load(infile)
+
 # get HTML page soup
-url = 'https://www.baseball-reference.com/teams/BOS/2018-schedule-scores.shtml'
-page = requests.get(url)
-soup = bs(page.content, 'html.parser')
+for team_url in team_urls:
 
-# make folder for storing results
-team_abbrev = url.split('.')[-2].split('/')[-2]
-output_folder = os.path.join('../data/', team_abbrev)
+    # make folder for storing results
+    team_abbrev = team_url.split('.')[-1].split('/')[-1]
+    output_folder = os.path.join('../data/', team_abbrev)
+    url = (
+        'https://www.baseball-reference.com/teams/' +
+        team_abbrev + '/2018-schedule-scores.shtml'
+    )
+    print(url)
 
-try:
-    os.makedirs(output_folder)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
+    # get page
+    page = requests.get(url)
+    soup = bs(page.content, 'html.parser')
 
-# find and convert table
-table_wrapper = soup.find('div', {'id': 'all_team_schedule'})
-table_html = str(table_wrapper.find('table'))
-df_table = pd.read_html(table_html)[0]
+    try:
+        os.makedirs(output_folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
-table_output_path = os.path.join(output_folder, 'season_summary.csv')
-df_table.to_csv(table_output_path)
+    # find and convert table
+    table_wrapper = soup.find('div', {'id': 'all_team_schedule'})
+    table_html = str(table_wrapper.find('table'))
+    df_table = pd.read_html(table_html)[0]
 
-# get links to each game summary
-boxscore_elems = soup.find_all('td', {'data-stat': 'boxscore'})
-boxscore_url_suffixes = [box_elem.find('a')['href'] for box_elem in boxscore_elems]
+    table_output_path = os.path.join(output_folder, 'season_summary.csv')
+    df_table.to_csv(table_output_path)
 
-base_url = '/'.join(url.split('/')[:3])
-boxscore_urls = ['/'.join([base_url, suffix]) for suffix in boxscore_url_suffixes]
+    # get links to each game summary
+    boxscore_elems = soup.find_all('td', {'data-stat': 'boxscore'})
+    boxscore_url_suffixes = [box_elem.find('a')['href'] for box_elem in boxscore_elems]
 
-boxscore_urls_path = os.path.join(output_folder, 'boxscore_urls.json')
-with open(boxscore_urls_path, 'w') as outfile:
-    json.dump(boxscore_urls, outfile)
+    base_url = '/'.join(url.split('/')[:3])
+    boxscore_urls = ['/'.join([base_url, suffix]) for suffix in boxscore_url_suffixes]
+
+    boxscore_urls_path = os.path.join(output_folder, 'boxscore_urls.json')
+    with open(boxscore_urls_path, 'w') as outfile:
+        json.dump(boxscore_urls, outfile)
